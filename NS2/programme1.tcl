f10a5289238812d447a42c19c787efa9e36fa343
Mati ul Rehman <iammatiz00&gmail.com>
2022-05-25 00:00:42 +0500

    NS2 added;

---
+++ NS2/programme1.tcl
@@ -0,0 +1,43 @@
+set ns [new Simulator]
+
+set nf [open output.nam w]
+
+$ns namtrace-all $nf
+
+#Creating Nodes
+
+set n0 [$ns node]
+
+set n1 [$ns node]
+
+set n2 [$ns node]
+
+set n3 [$ns node]
+
+#create Connection
+
+$ns duplex-link $n0 $n2 20Mb 10ms DropTail
+
+
+$ns duplex-link $n1 $n2 20Mb 10ms DropTail
+
+
+$ns duplex-link $n2 $n3 20Mb 10ms DropTail
+
+proc finish {} {
+
+global ns nf
+
+$ns flush-trace
+
+close $nf
+
+exec nam output.nam &
+
+exit 0
+
+}
+
+$ns at 20.0 "finish"
+
+$ns run
